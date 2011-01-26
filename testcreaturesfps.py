078ba9892dccb5d4f9170177c88f09ef9ede63a5
tps12 <tps12@columbia.edu>
2011-01-26 10:19:06 -0500

    Show number of guys.

--- testcreaturesfps.py
+++ testcreaturesfps.py
@@ -9,12 +9,14 @@ from rendering import Renderer
 from data import Dwarf
 
 def main():
-    print 'How fast can a world with lots of guys run.'
+    n = 2000
+    
+    print 'How fast can a world with {0} guys run.'.format(n)
     
     game = Game((256,256,128))
     renderer = Renderer(game)
 
-    for i in range(2000):
+    for i in range(n):
         game.world.creatures.append(Dwarf((randint(0,255),randint(0,255),64)))
     
     while not game.done:
