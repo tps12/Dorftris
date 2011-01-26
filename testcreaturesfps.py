31a3940c936e6a8b5864541866d2e04c316c5374
tps12 <tps12@columbia.edu>
2011-01-26 09:27:08 -0500

    FPS test case, gets around low-20's FPS.

---
+++ testcreaturesfps.py
@@ -0,0 +1,25 @@
+import gettext
+gettext.install('dorftris')
+
+from random import randint
+
+from game import Game
+from rendering import Renderer
+
+from data import Dwarf
+
+def main():
+    print 'How fast can a world with lots of guys run.'
+    
+    game = Game((256,256,128))
+    renderer = Renderer(game)
+
+    for i in range(2000):
+        game.world.creatures.append(Dwarf((randint(0,255),randint(0,255),64)))
+    
+    while not game.done:
+        game.step()
+        renderer.step()
+
+if __name__ == '__main__':
+    main()
