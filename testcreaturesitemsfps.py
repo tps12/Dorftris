61be9e9d05d189c0a026289448f6f64b2f58c360
tps12 <tps12@columbia.edu>
2011-01-26 16:47:13 -0500

    Item and creature test.

--- testcreaturesitemsfps.py
+++ testcreaturesitemsfps.py
@@ -7,12 +7,13 @@ from time import time
 from game import Game
 from rendering import Renderer
 
-from data import Barrel, Oak
+from data import Barrel, Dwarf, Oak
 
 def main():
-    n = 10000
+    n = 30000
+    m = 1500
     
-    print 'How fast can a world with {0} items run.'.format(n)
+    print 'How fast can a world with {0} items and {1} creatures run.'.format(n, m)
     
     game = Game((256,256,128))
     renderer = Renderer(game)
@@ -21,6 +22,11 @@ def main():
         b = Barrel((randint(0,255),randint(0,255),64), Oak)
         game.world.items.append(b)
         game.world.space[b.location].items.append(b)
+
+    for i in range(m):
+        d = Dwarf((randint(0,255),randint(0,255),64))
+        game.world.creatures.append(d)
+        game.world.space[d.location].contents.append(d)
 
     start = time()
     
