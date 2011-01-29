4162a4fd0d864e751eb0975c89d92238f64813b0
tps12 <tps12@columbia.edu>
2011-01-29 13:56:37 -0500

    Button has rect.

---
+++ button.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame import display, event, font, gfxdraw, key, mouse, Rect, Surface
+from pygame.locals import *
+from pygame.sprite import *
+
+class Button(object):
+    def __init__(self, font, text, click):
+        self.font = font
+        self.text = text
+        self.click = click
+        self.location = None
+
+    def draw(self, screen):
+        if self.location:
+            s = self.font.render(self.text, True, (255,255,255))
+            size = s.get_size()
+            self.rect = Rect([self.location[i]-size[i]/2 for i in range(2)],
+                             size)
+            screen.blit(s, self.rect.topleft)
+
+    @property
+    def size(self):
+        return self.rect.size
+    
+    def handle(self, e):
+        if e.type == MOUSEBUTTONUP and e.button == 1:
+            self.click()
