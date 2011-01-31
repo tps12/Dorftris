c95d628e3988d3c743b036f1dc342bc35d53110c
tps12 <tps12@columbia.edu>
2011-01-31 13:43:43 -0500

    Info pane.

--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 from pygame.locals import *
 
 from details import CreatureDetails
+from info import InfoView
 from playfield import Playfield
 
 class GameScreen(object):
@@ -9,14 +10,17 @@ class GameScreen(object):
     def __init__(self, game, font, zoom):
         self._game = game
         self._playfield = Playfield(self._game, font, zoom, [])
+        self._info = InfoView(self._playfield.selection, font)
         self.scale(font)
 
     def scale(self, font):
         self._font = font
         self._playfield.scale(self._font)
+        self._info.scale(self._font)
         
     def resize(self, size):
-        self._playfield.resize(size)
+        self._playfield.resize((size[0] - self._info.width, size[1]))
+        self._info.resize(size)
 
     def handle(self, e):
         if e.type == KEYDOWN:
@@ -24,10 +28,11 @@ class GameScreen(object):
                 return True, CreatureDetails(self._game.world.creatures[0],
                                              self._font)
 
-        if self._playfield.handle(e):
+        if self._playfield.handle(e) or self._info.handle(e):
             return True, self
         
         return False, self
                     
     def draw(self, surface):
         self._playfield.draw(surface)
+        self._info.draw(surface)
