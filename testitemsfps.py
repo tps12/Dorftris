bdaaac7ab27b041ee961e87831e5ff7947ba95de
tps12 <tps12@columbia.edu>
2011-02-03 13:06:28 -0500

    Item count test works.

--- testitemsfps.py
+++ testitemsfps.py
@@ -1,47 +1,71 @@
 import gettext
 gettext.install('dorftris')
 
-from random import randint
-from time import time
+from random import choice, randint
+from time import sleep, time
 
+import pygame
+from pygame.locals import *
+from pygame import display
+
+from data import Pickax, Player
 from game import Game
+from prefs import DisplayOptions
 from rendering import Renderer
-
-from data import Barrel, Item
+from substances import Metal, Wood
 
 def main():
     n = 250000
     
     print 'How fast can a world with {0} items run.'.format(n)
+
+    pygame.init()
     
     game = Game((256,256,128))
-    renderer = Renderer(game)
+    user = Player(game.world)
 
     for i in range(n):
-        game.world.additem(Item(Barrel, (randint(0,255),randint(0,255),64), 1.0))
+        game.world.additem(Pickax((randint(0,255),randint(0,255),64),
+                                  choice(Metal.__subclasses__()),
+                                  choice(Wood.__subclasses__())))
 
+    display.set_mode((1400,800), HWSURFACE | RESIZABLE)
+
+    renderers = [Renderer(game, user, DisplayOptions('FreeMono.ttf', False, 16, 18))]
+    
     game_acc = 0
     render_acc = 0
 
-    render_dt = 0.05
-
     last = time()
-    
-    while not game.done:
+
+    while renderers:
         current = time()
         delta = min(0.125, max(0, current - last))
 
-        game_acc += delta
+        renderer = renderers[-1]   
+        
+        if renderer.game:
+            game_acc += delta
+            while game_acc > renderer.game.dt:
+                renderer.game.step()
+                game_acc -= renderer.game.dt
+            rest = renderer.game.dt - game_acc
+        else:
+            rest = float('inf')
+        
         render_acc += delta
-
-        while game_acc > game.dt:
-            game.step()
-            game_acc -= game.dt
-        if render_acc > render_dt:
-            renderer.step()
+        if render_acc > renderer.dt:
+            child = renderer.step()
+            if child != renderer:
+                if child:
+                    renderers.append(child)
+                else:
+                    renderers = renderers[:-1]
             render_acc = 0
 
         last = current
+
+        sleep(min(rest, renderers[-1].dt if renderers else 0))
 
 if __name__ == '__main__':
     main()
