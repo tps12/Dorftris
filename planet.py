a807a887d4377d8d0ade551829a5c72891f19ccc
tps12 <tps12@columbia.edu>
2011-02-13 08:32:00 -0500

    Compose multiple noises.

--- planet.py
+++ planet.py
@@ -8,7 +8,17 @@ class Planet(object):
         c = cos(lat)
         x, y, z = c * cos(lon), c * sin(lon), sin(lat)
 
-        octaves = 10
+        octaves = 6
         persist = 0.7
 
-        return 9000 * pnoise3(x, y, z, octaves, persist)
+        b = 1 if pnoise3(x+18, y-3, z+3, 4, persist) > -0.125 else -1
+        value = pnoise3(x-3, y+2, z, octaves, persist)
+        if value > 0.25:
+            value *= 1.25 * b
+        elif value > 0:
+            value *= b
+        else:
+            value *= 3
+
+        scale = 5000
+        return scale * value
