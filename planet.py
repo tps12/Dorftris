608cfe20e5a46cc4deda0f5594cc79bd89ff21d5
tps12 <tps12@columbia.edu>
2011-02-17 18:29:57 -0500

    Randomize button.

--- planet.py
+++ planet.py
@@ -3,6 +3,9 @@ from math import cos, pi, sin
 from noise import pnoise3
 
 class Planet(object):
+    def __init__(self):
+        self.seeds = 18, -3, 3, -3, 2, 0
+        
     def sample(self, latitude, longitude):
         lat, lon = [v*pi/180 for v in latitude, longitude]
         c = cos(lat)
@@ -11,8 +14,12 @@ class Planet(object):
         octaves = 12
         persist = 0.7
 
-        b = 1 if pnoise3(x+18, y-3, z+3, 4, persist) > -0.125 else -1
-        value = pnoise3(x-3, y+2, z, octaves, persist)
+        b = 1 if pnoise3(x+self.seeds[0],
+                         y+self.seeds[1],
+                         z+self.seeds[2], 4, persist) > -0.125 else -1
+        value = pnoise3(x+self.seeds[3],
+                        y+self.seeds[4],
+                        z+self.seeds[5], octaves, persist)
         if value > 0.25:
             value *= 1.25 * b
         elif value > 0:
--- planetsettings.py
+++ planetsettings.py
@@ -1,7 +1,10 @@
 from math import acos, asin, atan2, pi, sqrt
+from random import randint
 
 from pygame import display, draw, event, font, key, Rect, Surface
 from pygame.locals import *
+
+from button import Button
 
 class PlanetSettings(object):
     def __init__(self, zoom, planet):
@@ -13,16 +16,34 @@ class PlanetSettings(object):
         self.definetiles()
 
         self.selection = None
+        self._buttons = []
 
     def definetiles(self):
         self.uifont = self.zoom.font
                 
     def makebackground(self, surface):
         surface.fill((0,0,0))
-        surface.blit(self.uifont.render('planet settings...', True, (255,255,255)),
-                     (0,0))
+        
+        self._buttons = [
+            Button(self.uifont, _(u'Randomize'), self._randomize)
+            ]
+
+        size = surface.get_size()
+        y = 0
+        for b in self._buttons:
+            b.location = (0, y)
+            b.draw(surface)
+            y += b.size[1]
+
+    def _randomize(self):
+        self.planet.seeds = tuple([randint(-3,3)
+                                   for i in range(len(self.planet.seeds))])
         
     def handle(self, e):
+        for b in self._buttons:
+            if b.handle(e):
+                return True
+        
         return False
                                  
     def draw(self, surface):
