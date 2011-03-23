a921d913c8dea3134c116e247a4d582687734821
tps12 <tps12@columbia.edu>
2011-03-23 09:13:16 -0400

    Make a society.

--- factions.py
+++ factions.py
@@ -4,3 +4,7 @@ class Faction(object):
         self.color = color
         self.status = status
         self.values = dict([(v, 0.5) for v in values])
+
+class Society(object):
+    def __init__(self, factions):
+        self.factions = factions
--- regimes.py
+++ regimes.py
@@ -5,6 +5,8 @@ from pygame import display, draw, event, mouse, Rect, Surface
 from pygame.locals import *
 from pygame.sprite import *
 
+from factions import *
+
 pygame.init()
 
 screen = display.set_mode((800,600),HWSURFACE)
@@ -14,6 +16,11 @@ background = Surface(screen.get_size())
 background.fill((128,128,128))
 
 screen.blit(background, (0,0))
+
+values = 'militarism', 'moralism'
+society = Society([Faction('aristocracy', (0,0,255), random(), values),
+                   Faction('merchant class', (0,255,0), random(), values),
+                   Faction('populace', (255,0,0), random(), values)])
 
 done = False
 
