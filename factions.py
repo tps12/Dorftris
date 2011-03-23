ebd1b39f883ba039e9c82ad5c46386c49775fe93
tps12 <tps12@columbia.edu>
2011-03-23 10:39:20 -0400

    Agent-based.

--- factions.py
+++ factions.py
@@ -5,41 +5,31 @@ class Faction(object):
         self.status = status
         self.values = dict([(v, 0.5) for v in values])
 
-def stasis(society):
-    return stasis
+    def iterate(self, others):
+        if self.status == 1:
+            # ruling
+            threat = max(others, key=lambda f: f.status)
+            if threat.status > 0.5:
+                # repress potential threat
+                target = threat
+            else:
+                # repress underclass
+                target = min(others, key=lambda f: f.status)
 
-def regime(society):
-    ruler = next((f for f in society.factions if f.status == 1))
-    underclass = min(society.factions, key=lambda f: f.status)
-    d = min(0.01, underclass.status)
-
-    threat = next((f for f in society.factions
-                   if f != ruler and 1 - f.status < d),
-                  None)
-    target = threat or underclass
-
-    target.status -= d
-    for f in society.factions:
-        if f == ruler or f == target:
-            continue
-        f.status = min(f.status + d/(len(society.factions)-2), 1)
-    
-    return regime
-
-def disarray(society):
-    ascendant = max(society.factions, key=lambda f: f.status)
-    d = min(0.01, 1 - ascendant.status)
-    ascendant.status += d
-    for f in society.factions:
-        if f == ascendant:
-            continue
-        f.status = max(f.status - d/(len(society.factions)-1), 0)
-    return disarray if ascendant.status < 1 else regime
+            d = min(0.001, target.status)
+            target.status -= d
+        elif all([self.status > f.status for f in others]):
+            # rise to power
+            d = min(0.001, 1 - self.status)
+            self.status += d
+            for f in others:
+                if f != self:
+                    f.status -= d/len(others)
 
 class Society(object):
     def __init__(self, factions):
         self.factions = factions
-        self.mode = disarray
 
     def iterate(self):
-        self.mode = self.mode(self)
+        for f in self.factions:
+            f.iterate([e for e in self.factions if e != f])
