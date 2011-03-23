b81bab2eb4245bad294ddab9fc1974e69f444014
tps12 <tps12@columbia.edu>
2011-03-23 11:18:55 -0400

    Overthrow diametrically opposed regime.

--- factions.py
+++ factions.py
@@ -4,9 +4,10 @@ class Faction(object):
         self.color = color
         self.status = status
         self.values = dict([(v, 0.5) for v in values])
+        self.elite = None
 
     def ossify(self):
-        d = 0.0001
+        d = 0.001
         for v in self.values.keys():
             if self.values[v] >= 0.5:
                 self.values[v] = min(1, self.values[v] + d)
@@ -20,6 +21,20 @@ class Faction(object):
                 self.values[v] = max(0, self.values[v] - d)
             else:
                 self.values[v] = min(1, self.values[v] + d)
+        if all([abs(self.values[v] - ruler.values[v]) == 1
+                for v in self.values.keys()]):
+            self.elite = ruler
+
+    def conform(self, ruler):
+        d = 0.001
+        for v in self.values.keys():
+            if ruler.values[v] >= 0.5:
+                self.values[v] = min(1, self.values[v] + d)
+            else:
+                self.values[v] = max(0, self.values[v] - d)
+        if all([abs(self.values[v] - ruler.values[v]) == 1
+                for v in self.values.keys()]):
+            self.elite = ruler
 
     def repress(self, target):
         d = min(0.001, target.status)
@@ -43,6 +58,18 @@ class Faction(object):
         if ruler.status == 1:
             self.react(ruler)
 
+    def muddle(self, others):
+        ruler = max(others, key=lambda f: f.status)
+        if ruler.status == 1:
+            self.conform(ruler)
+
+    def overthrow(self):
+        d = min(0.01, self.elite.status - self.status)
+        self.status += d
+        self.elite.status -= d
+        if self.status > self.elite.status:
+            self.elite = None
+
     def struggle(self, others):
         d = min(0.001, 1 - self.status)
         self.status += d
@@ -50,12 +77,16 @@ class Faction(object):
             f.status -= d/len(others)
 
     def iterate(self, others):
-        if self.status == 1:
+        if self.elite:
+            self.overthrow()
+        elif self.status == 1:
             self.rule(others)
         elif self.status == 0:
             self.wallow(others)
         elif all([self.status > f.status for f in others]):
             self.struggle(others)
+        else:
+            self.muddle(others)
             
 class Society(object):
     def __init__(self, factions):
