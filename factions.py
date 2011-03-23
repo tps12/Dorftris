404aebd83c4061a872a531fbc0350ef291d8e3f7
tps12 <tps12@columbia.edu>
2011-03-23 10:03:37 -0400

    Oppress threat or underclass.

--- factions.py
+++ factions.py
@@ -8,6 +8,24 @@ class Faction(object):
 def stasis(society):
     return stasis
 
+def regime(society):
+    ruler = next((f for f in society.factions if f.status == 1))
+    underclass = min(society.factions, key=lambda f: f.status)
+    d = min(0.01, underclass.status)
+
+    threat = next((f for f in society.factions
+                   if f != ruler and 1 - f.status < d),
+                  None)
+    target = threat or underclass
+
+    target.status -= d
+    for f in society.factions:
+        if f == ruler or f == target:
+            continue
+        f.status = min(f.status + d/(len(society.factions)-2), 1)
+    
+    return regime
+
 def disarray(society):
     ascendant = max(society.factions, key=lambda f: f.status)
     d = min(0.01, 1 - ascendant.status)
@@ -16,7 +34,7 @@ def disarray(society):
         if f == ascendant:
             continue
         f.status = max(f.status - d/(len(society.factions)-1), 0)
-    return disarray
+    return disarray if ascendant.status < 1 else regime
 
 class Society(object):
     def __init__(self, factions):
