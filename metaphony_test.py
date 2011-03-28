c1183bfde8956e8a90188deb6233ef66f24ed487
tps12 <tps12@columbia.edu>
2011-03-27 23:26:09 -0400

    Tests.

--- metaphony_test.py
+++ metaphony_test.py
@@ -11,5 +11,28 @@ class MetaphonyTestCase(unittest.TestCase):
         wm = i_mutate(w)
         self.assertNotEqual(w, wm)
 
+    def test_defeeties(self):
+        w = Word([Syllable(*s)
+                  for s in [[['d'],['e'],[]],
+                            [['f'],['e'],['t']],
+                            [[],['i'],['z']]]])
+        wm = i_mutate(w)
+        self.assertNotEqual(w, wm)
+
+    def test_feetieso(self):
+        w = Word([Syllable(*s)
+                  for s in [[['f'],['e'],['t']],
+                            [[],['i'],['z']],
+                            [[],['o'],[]]]])
+        wm = i_mutate(w)
+        self.assertNotEqual(w, wm)
+
+    def test_feetoes(self):
+        w = Word([Syllable(*s)
+                  for s in [[['f'],['e'],['t']],
+                            [[],['o'],['z']]]])
+        wm = i_mutate(w)
+        self.assertEqual(w, wm)
+
 if __name__ == '__main__':
     unittest.main()
