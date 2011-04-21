c263700b903e3fa552d28563c273cf9e3dca87c6
tps12 <tps12@columbia.edu>
2011-04-21 07:50:57 -0400

    Remove scratch dict.

--- climate.py
+++ climate.py
@@ -124,8 +124,7 @@ class ClimateSimulation(object):
                 dump(self.adj, f, 0)
 
         xmax = max([len(self.tiles[i]) for i in range(len(self.tiles))])
-        self.climate, self._scratch = [ClimateDict((xmax, len(self.tiles)))
-                                       for i in range(2)]
+        self.climate = ClimateDict((xmax, len(self.tiles)))
 
         self.dirty = True
 
